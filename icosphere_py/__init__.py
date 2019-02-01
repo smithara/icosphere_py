cf85941813d1fe68154bb54c5ec9530433a17e02
Ashley Smith <ashley.smith@ed.ac.uk>
2019-02-01 16:16:06 +0000

    Add basic Python package structure.

---
+++ icosphere_py/__init__.py
@@ -0,0 +1,3 @@
+from . import utils
+
+__version__ = "0.0.1"
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('icosphere/__init__.py') as f:
+    for line in f:
+        if line.find("__version__") >= 0:
+            version = line.split("=")[1].strip()
+            version = version.strip('"')
+            version = version.strip("'")
+            continue
+
+setup(
+    name='icosphere_py',
+    packages=find_packages(),
+    version=version,
+    description='Generating a spherical grid',
+    url='https://github.com/smithara/icosphere_py',
+    author='Ashley Smith',
+    author_email='ashley.smith@ed.ac.uk',
+    license='MIT',
+    install_requires=['']
+)
